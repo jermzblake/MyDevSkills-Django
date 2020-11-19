c2f35b967b2badc0c3ef5ff22add09b26d7009ac
Jermaine <jermzblake@gmail.com>
2020-11-19 12:53:49 -0500

    add Skill model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+LEVELS = (
+    (1, 'Fundamental Awareness'),
+    (2, 'Novice'),
+    (3, 'Intermediate'),
+    (4, 'Advanced'),
+    (5, 'Expert')
+)
+
+class Skill(models.Model):
+    description = models.TextField(max_length=250)
+    skill_level = models.IntegerField(choices=LEVELS, default=LEVELS[0][0])
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
