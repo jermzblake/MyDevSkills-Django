d3c64f5c7e773961c2606495d256d619cc04e660
Jermaine <jermzblake@gmail.com>
2020-11-19 22:34:54 -0500

    add signup view and html

--- MyDevSkills/settings.py
+++ MyDevSkills/settings.py
@@ -128,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = ''
+# Add this variable to specify where logging out redirects to
+LOGOUT_REDIRECT_URL = '/'
